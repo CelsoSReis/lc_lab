b196ebfd2595a5ba2740161b8572849d1c0b6e28
Celso Reis <celsosr87@gmail.com>
2024-11-16 11:26:50 -0300

    Finalizando Solicitação de exames

--- exames/views.py
+++ exames/views.py
@@ -1,21 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import TiposExames
+from .models import TiposExames, PedidosExames, SolicitacaoExame
+from datetime import datetime
+from django.contrib import messages
+from django.contrib.messages import constants
 
 @login_required
 def solicitar_exames(request):
+    #Lista exames no select para o usuário selecionar
     tipos_exames = TiposExames.objects.all()
     if request.method == "GET":
         return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
     elif request.method == "POST":
+        # atributo "getlist" seleciona vários valores, "get" somente um valor
         exames_id = request.POST.getlist('exames')
-
+        # verifica no banco de dados informações dos exames selecionados pelo usuário
         solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
         #preco_total = solicitacao_exames.aggregate(total=Sum('preco'))['total']
+        ## Calcula valor total dos exames selecionados pelo usuário
         preco_total = 0
         for i in solicitacao_exames:
+            #Condição soma preço só se estiver disponível
             if i.disponivel:
                 preco_total += i.preco
         
         return render(request, 'solicitar_exames.html', {'solicitacao_exames': solicitacao_exames, 'preco_total': preco_total, 'tipos_exames': tipos_exames})
     
+@login_required
+def fechar_pedido(request):
+    exames_id = request.POST.getlist('exames')
+    #busca tipos de exames
+    solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+    #pedido de exame
+    pedido_exame = PedidosExames(
+        usuario = request.user,
+        data = datetime.now()
+    )
+    #método save(), salva no banco de dados
+    pedido_exame.save()
+
+    for exame in solicitacao_exames:
+        solicitacao_exames_temp = SolicitacaoExame(
+            usuario=request.user,
+            exame=exame,
+            status="E"
+        )
+        solicitacao_exames_temp.save()
+        pedido_exame.exames.add(solicitacao_exames_temp)
+        
+    pedido_exame.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Pedido de exame concluído com sucesso')
+    return redirect('/exames/ver_pedidos/')
