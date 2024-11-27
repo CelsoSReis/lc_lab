78848f63cd87772f45e0e7c29c18ee872fbca13d
Celso Reis <celsosr87@gmail.com>
2024-11-27 16:17:33 -0300

    Update Urls

--- lclab/urls.py
+++ lclab/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from usuarios import views
 #URLs do Sistema
 urlpatterns = [
+    path('', views.logar, name="logar"),
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
     path('exames/', include('exames.urls')),
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro/', views.cadastro, name="cadastro"),
     path('login/', views.logar, name="login"),
-]
+    path('cadastro/', views.cadastro, name="cadastro"),
+    ]
